bddabbde4510086479920afe03a89d64df24d93a
phantom <phantomdesign9@gmail.com>
2019-05-01 21:31:46 -0500

    erae

--- config/connection.js
+++ config/connection.js
@@ -6,29 +6,18 @@ var mysql = require("mysql");
 //==============INITIATE THE CONNECTION==============
 
 var connection;
- connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-// connection = mysql.createConnection(process.env.JAWSDB_URL ||
-//     {
-//         host: "localhost",
-//         port: "3306",
-//         user: "root",
-//         password: "root",
-//         database: "burger_db"
-//     });
-
-
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL)
-// } else {
-//     connection = mysql.createConnection({
-//         host: "localhost",
-//         port: "3306",
-//         user: "root",
-//         password: "root",
-//         database: "burger_db"
-//     });
-// };
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: "3306",
+        user: "root",
+        password: "root",
+        database: "burger_db"
+    });
+};
 
 /*
 var connection = mysql.createConnection({
